26df27ca02bbbef5b116d50d089db26554bd5096
Shahmeer-Ahmed-2008 <programmer17102008@gmail.com>
2024-11-21 21:12:23 -0800

    Initial commit

---
+++ first/src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="bg-cyan-400 h-screen">
+    <h1 className="text-7xl text-center font-bold py-64">
+      HELLO WORLD!
+    </h1>
+    <h2 className="text-2xl text-center">
+      I am "Shahmeer Ahmed".
+    </h2>
+  </div>
+  )
+}
